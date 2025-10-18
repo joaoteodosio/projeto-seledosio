5f8eb6a5332f0f8bb8e4254ea829093f9acd8537
joao maria teodosio <jtheodosio17@gmail.com>
2025-10-18 04:39:39 -0300

    atualizado seção fotos

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ function toggleMenu() {
         }
     });
 }
+// SLIDER
+let currentIndex = 0;
+const slides = document.querySelectorAll(".slide");
+const totalSlides = slides.length;
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+function showSlide(index) {
+    const slidesContainer = document.querySelector(".slides");
+    if (index >= totalSlides) currentIndex = 0;
+    else if(index < 0) currentIndex = totalSlides - 1;
+    else currentIndex = index;
+
+    slidesContainer.style.transform =  `translateX(-${currentIndex * 100}%)`;
+}
+
+nextBtn.addEventListener("clik", ()=> showSlide(currentIndex + 1));
+prevBtn.addEventListener("clik", ()=> showSlide(currentIndex - 1));
+
+setInterval(()=> {
+    showSlide(currentIndex + 1);
+}, 5000); // troca automatica a cada 5s
